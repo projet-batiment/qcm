953f09b26a35f11a46a6f42f6697371ed1cfd805
Gaspard-R <gaspard.regent@gmail.com>
2026-01-29 22:50:06 +0100

    qcm edit for bdd

--- src/model/qcm.py
+++ src/model/qcm.py
@@ -1,6 +1,16 @@
-class Qcm:
-    def __init__(self, titre: str, liste_questions=None):
-        self.titre = titre
+from typing import List, Optional
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from src.model.bdd_init import Base
+
+class Qcm(Base):
+    __tablename__ = 'qcm'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    titre = Column(String, nullable=False)
+    liste_questions = relationship("Question", back_populates="qcm", cascade="all, delete-orphan")
+
+    def __init__(self, titre: str, liste_questions: Optional[List] =None):
+        self.titre=titre
         if liste_questions is None:
             self.liste_questions = []
         else:
