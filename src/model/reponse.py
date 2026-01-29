7c1120750673466aef670e6785e4e9060599342b
Gaspard-R <gaspard.regent@gmail.com>
2026-01-29 22:59:27 +0100

    reponse edit for bdd

--- src/model/reponse.py
+++ src/model/reponse.py
@@ -2,13 +2,14 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from src.model.bdd_init import Base
 
+
 class Reponse(Base):
-    __tablename__ = 'reponses'
+    __tablename__ = "reponses"
     id = Column(Integer, primary_key=True, autoincrement=True)
     texte = Column(String, nullable=False)
     est_correcte = Column(Boolean, default=False)
     feedback = Column(String, default="")
-    question_id = Column(Integer, ForeignKey('questions_qcm.id'))
+    question_id = Column(Integer, ForeignKey("questions_qcm.id"))
     question = relationship("QuestionQCM", back_populates="choix_rep")
 
     def __init__(self, texte: str, est_correcte: bool = False, feedback: str = ""):
