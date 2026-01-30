64279cc4140012e5dcedd4359a8a1bc3a5071f22
aeich7 <eich.aurelien@orange.fr>
2026-01-30 09:46:04 +0100

    feat Modif Structure Question + Implementation QCUnique + Adaptation Reponse pour CU

--- src/model/question.py
+++ src/model/question.py
@@ -1,35 +1,41 @@
-from abc import ABC, abstractmethod
+from typing import List
 
 
-class Question(ABC):
-    def __init__(self, enonce: str, points: int = 1) -> None:
+class Question:
+    def __init__(self, enonce: str, points: int = 1):
         self.enonce = enonce
         self.points = points
 
-    @abstractmethod
-    def verifier(self, proposition) -> bool:
-        """Vérifie si la proposition donnée est correcte."""
-        pass
 
-
-class QuestionQCM(Question):
+class QuestionQCMultiples(Question):
     def __init__(
-        self, enonce: str, points: int, choix_rep: list[str], id_bonne_reponse: int
+        self,
+        enonce: str,
+        points: int,
+        choix_rep: List[str],
+        id_bonne_reponse: List[int],
     ):
+        """
+        :param id_bonne_reponse: Une LISTE d'indices corrects (ex: [0] ou [0, 2])
+        """
         super().__init__(enonce, points)
         self.choix_rep = choix_rep
         self.id_bonne_reponse = id_bonne_reponse
 
-    def verifier(self, index_utilisateur: int) -> bool:
-        """Compare l'index choisi avec l'index attendu."""
-        return index_utilisateur == self.id_bonne_reponse
+
+class QuestionQCUnique(Question):
+    def __init__(
+        self, enonce: str, points: int, choix_rep: List[str], id_bonne_reponse: int
+    ):
+        """
+        :param id_bonne_reponse: Un ENTIER de l'indice correct
+        """
+        super().__init(enonce, points)
+        self.choix_rep = choix_rep
+        self.id_bonne_reponse = id_bonne_reponse
 
 
 class QuestionLibre(Question):
     def __init__(self, enonce: str, points: int, rep_attendue: str):
         super().__init__(enonce, points)
         self.rep_attendue = rep_attendue
-
-    def verifier(self, texte_utilisateur: str) -> bool:
-        """Compare les chaînes de caractères (insensible à la casse)."""
-        return texte_utilisateur.strip().lower() == self.rep_attendue.lower()
