ee628f03f00f28121fc0b9d5f5678f8e6aacead4
bowan71 <erwan.bore71@gmail.com>
2026-02-11 15:42:20 +0100

    Update logs.py

--- src/qcm/utils/logs.py
+++ src/qcm/utils/logs.py
@@ -2,9 +2,7 @@ import logging
 
 
 def setup_logging():
-    """Configure le logging pour toute l'application."""
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
-    
